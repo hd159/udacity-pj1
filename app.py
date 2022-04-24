66aced6e5b08ac383b150e18a65785e647561d7f
Quan Nguyen <quan.nguyen3@halliburton.com>
2022-04-24 18:12:33 +0700

    add flask migrate

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+from flask_migrate import Migrate
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -20,43 +21,69 @@ app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-
+migrate = Migrate(app, db)
 # TODO: connect to a local postgresql database
 
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
-
 class Venue(db.Model):
     __tablename__ = 'Venue'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    genres = db.Column(db.String)
+    address = db.Column(db.String(120))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
+    website = db.Column(db.String(120))
     facebook_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String)
+    image_link = db.Column(db.String(500))
+    # past_shows = db.Column(db.String)
+    # upcoming_shows = db.Column(db.String)
+    # past_shows_count = db.Column(db.Number)
+    # upcoming_shows_count = db.Column(db.Number)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
 class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    genres = db.Column(db.String(120))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
+    website = db.Column(db.String(120))
     facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String)
+    image_link = db.Column(db.String(500))
+    # past_shows = db.Column(db.String(500))
+    # upcoming_shows = db.Column(db.String(500))
+    # past_shows_count = db.Column(db.String(500))
+    # upcoming_shows_count = db.Column(db.String(500))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+db.create_all()
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+    __tablename__ = 'Show'
 
+    id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime)
+
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'),
+                         nullable=False)
+    venue = db.relationship('Venue', lazy=True)
+
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'),
+                          nullable=False)
+    artist = db.relationship('Artist', lazy=True)
 #----------------------------------------------------------------------------#
 # Filters.
 #----------------------------------------------------------------------------#
---
+++ migrations/versions/0e025f4f00e8_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 0e025f4f00e8
+Revises: 
+Create Date: 2022-04-24 18:05:40.591623
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0e025f4f00e8'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
